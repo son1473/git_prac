2bc383ff97e10e98515f46799d2a2731446a0aa2
손명근 <zl6575@gmail.com>
2024-04-12 15:43:59 +0900

    update vending_machine files

--- vm_class.py
+++ vm_class.py
@@ -1,33 +1,47 @@
 class Vending_Machine:
-    '''Represents any school member.'''
+    def __init__(self, beverage): # 이게 컴포지션, 상속과 다르게 확장, 재사
+        self.beverage = beverage
+        print('(Initialized Vending_Machine: {})'.format(self.beverage))
+    def buy(self):
+        # 구매 행동
+        print('')
+    
+    def pay(self):
+        # 지불 행동
+        print('')
+
+class Beverage:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print('(Initialized Vending_Machine: {})'.format(self.name))
-    def tell(self):
-        '''Tell my details.'''
-        print('Name:{} Age:{}'.format(self.name, self.age), end=' ')
-class Teacher(Vending_Machine): # 이게 상ㅅ옥
+
+class Teacher(Person): # 이게 상속
     '''Represents a teacher.'''
     def __init__(self, name, age, salary):
-        Vending_Machine.__init__(self, name, age) 
+        Person.__init__(self, name, age) 
         self.salary = salary
         print('(Initialized Teacher: {})'.format(self.name))
     def tell(self):
-        Vending_Machine.tell(self)
+        Person.tell(self)
         print('Salary: {:d}'.format(self.salary))
-class Student(Vending_Machine):
+class Student(Person):
     '''Represents a student.'''
     def __init__(self, name, age, marks):
-        Vending_Machine.__init__(self, name, age)
+        Person.__init__(self, name, age)
         self.marks = marks
         print('(Initialized Student: {})'.format(self.name))
     def tell(self):
-        Vending_Machine.tell(self)
+        Person.tell(self)
         print('Marks: {:d}'.format(self.marks))
 t = Teacher('Mrs. Shrividya', 40, 30000)
 s = Student('Swaroop', 25, 75)
-# prints a blank line
+
 print()
 members = [t, s]
 for member in members:
